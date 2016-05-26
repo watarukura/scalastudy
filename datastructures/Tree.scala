5d1fbb492261d0038a3b4241ac45ea71a3342172
wataru-kurashima <wataru-kurashima@hands-lab.com>
2016-05-26 17:58:07 +0900

    2016/05/26 learning-scala

--- datastructures/Tree.scala
+++ datastructures/Tree.scala
@@ -20,9 +20,26 @@ object Tree {
     case Branch(l, r) => depth(l) max depth(r) + 1
   }
 
-  def map[A](t: Tree[A])(f): Int = t match {
-    case Leaf(_) => 0
-    case Branch(l, r) => depth(l) max depth(r) + 1
+  def map[A, B](t: Tree[A])(f: A=>B): Tree[B] = t match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
   }
+
+  def fold[A,B](t: Tree[A])(l: A => B)(b: (B,B) => B): B = t match {
+    case Leaf(v) => l(v)
+    case Branch(left, right) => b(fold(left)(l)(b), fold(right)(l)(b))
+  }
+
+  def size2[A](t: Tree[A]): Int =
+    fold(t)(_ => 1)(_ + _ + 1)
+
+  def maximum2(t: Tree[Int]): Int =
+    fold(t)(v => v)((l, r) => l max r)
+
+  def depth2[A](t: Tree[A]): Int =
+    fold(t)(_ => 0)((l, r) => l+1 max r+1)
+
+  def map2[A,B](t: Tree[A])(f: A=>B): Tree[B] =
+    fold(t)(v => Leaf(f(v)): Tree[B])(Branch(_,_))
 }
 
