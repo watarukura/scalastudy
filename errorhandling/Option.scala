86f37e99b3ae41d429c5f3e2dd9daa2715a60d60
wataru-kurashima <wataru-kurashima@hands-lab.com>
2016-09-27 17:07:35 +0900

    第4章 Option型

---
+++ errorhandling/Option.scala
@@ -0,0 +1,27 @@
+sealed trait Option[+A]
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+object Option {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+}
+
+def failingFn(i: Int): Int = {
+  val y: Int = throw new Exception("fail!")
+  try {
+    val x = 42 + 5
+    x + y
+  }
+  catch { case e: Exception => 43 }
+}
+
+def failingFn2(i: Int): Int = {
+  try {
+    val x = 42 + 5
+    x + ((throw new Exception("fail!")): Int)
+  }
+  catch { case e: Exception => 43 }
+}
+
