0f60b5a52dc2234c51bb6976fc780aff7b1827f0
wataru-kurashima <wataru-kurashima@hands-lab.com>
2016-11-07 11:21:12 +0900

    2016/09/27 learning scala

--- errorhandling/Option.scala
+++ errorhandling/Option.scala
@@ -1,27 +1,40 @@
-sealed trait Option[+A]
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(a) => f(a)
+  }
+
+}
+
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
+
 
 object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-}
 
-def failingFn(i: Int): Int = {
-  val y: Int = throw new Exception("fail!")
-  try {
-    val x = 42 + 5
-    x + y
+  def failingFn(i: Int): Int = {
+    val y: Int = throw new Exception("fail!")
+    try {
+      val x = 42 + 5
+      x + y
+    }
+    catch { case e: Exception => 43 }
   }
-  catch { case e: Exception => 43 }
-}
 
-def failingFn2(i: Int): Int = {
-  try {
-    val x = 42 + 5
-    x + ((throw new Exception("fail!")): Int)
+  def failingFn2(i: Int): Int = {
+    try {
+      val x = 42 + 5
+      x + ((throw new Exception("fail!")): Int)
+    }
+    catch { case e: Exception => 43 }
   }
-  catch { case e: Exception => 43 }
 }
 
