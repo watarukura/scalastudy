969faec80fcdfb4ea50148352f8c674fbe88687b
wataru-kurashima <wataru-kurashima@hands-lab.com>
2017-01-11 17:24:21 +0900

    learning scala 2017-01-11

--- errorhandling/Option.scala
+++ errorhandling/Option.scala
@@ -68,5 +68,9 @@ object Option {
     }
   }
 
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h::t => h flatMap ( hh => sequence(t) map (hh::_))
+  }
 }
 
